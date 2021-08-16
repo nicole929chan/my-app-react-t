0808d25a7e4b08147fb0f253693f2b2abe0d33fe
nicole929chan <nicole929chan@gmail.com>
2021-08-16 13:22:10 +0800

    uniqueReducer

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,14 +1,33 @@
-import React, { useState } from 'react';
-import { useUnique } from './hooks/useUnique';
+import React, { useReducer, useState } from 'react';
+
+function uniqueReducer(state, action) {
+    switch (action.type) {
+        case 'add':
+            return [...new Set([...state, action.name])];
+        case 'delete':
+            return state.filter((name) => name === action.name);
+        default:
+            throw new Error();
+    }
+}
 
 const ProductList = () => {
-    const [names, add] = useUnique([]);
+    const [names, dispatch] = useReducer(uniqueReducer, []);
     const [newName, setNewName] = useState('');
 
     const map = (name, i) => <div key={i}>{name}</div>;
 
     const handleChange = (event) => setNewName(event.target.value);
-    const handleAdd = () => add(newName);
+    const handleAdd = () => dispatch({ type: 'add', name: newName });
+    // const map = name => {
+    //     const delete = () => dispatch({ type: 'delete', name });
+    //     return (
+    //       <div>
+    //         {name}
+    //         <button onClick={delete}>Delete</button>
+    //       </div>
+    //     );
+    //   }
 
     return (
         <div className='products'>
